e1d956a7ae768ba19bee71c3d99f55d23a5aba0d
anilmeister <anilkapikiran@hotmail.com>
2022-07-31 18:58:51 +0300

    Explosion and muzzle flash improvements.

--- 2DTank/Assets/Anıl/Shooting.cs
+++ 2DTank/Assets/Anıl/Shooting.cs
@@ -7,6 +7,8 @@ public class Shooting : MonoBehaviour
 
     public Transform firePoint;
     public GameObject bulletPrefab;
+    public GameObject muzzleFlash;
+
 
     public float bulletForce = 20f;
 
@@ -24,7 +26,9 @@ public class Shooting : MonoBehaviour
 
     void Shoot()
     {
-       GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+       GameObject effect = Instantiate(muzzleFlash, transform.position, Quaternion.identity);
+       Destroy(effect, 0.2f);
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
        rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
        
