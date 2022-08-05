11786e01a2e6aaafe71d5b124be0c0f48ebadbac
melihacil <knghtmellomello@gmail.com>
2022-08-05 15:50:36 +0300

    Updating level 1 and adding level 2

---
+++ 2DTank/Assets/Scripts/ExilLevel.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExilLevel : MonoBehaviour
+{
+    
+    GameManager gameManager;
+
+    private void Awake()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            gameManager.SaveData();
+            gameManager.LoadNextLevel();
+        }
+    }
+
+}
